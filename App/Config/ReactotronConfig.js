d00debfaaf4946c06630f5ffebf12cae39b8569e
515074431 <515074431@qq.com>
2018-08-31 15:00:37 +0800

    改名字

--- App/Config/ReactotronConfig.js
+++ App/Config/ReactotronConfig.js
@@ -7,7 +7,7 @@ import sagaPlugin from 'reactotron-redux-saga'
 if (Config.useReactotron) {
   // https://github.com/infinitered/reactotron for more options!
   Reactotron
-    .configure({ name: '花手领券 App' })
+    .configure({ name: '券大王 App' })
     .useReactNative()
     .use(reduxPlugin({ onRestore: Immutable }))
     .use(sagaPlugin())
