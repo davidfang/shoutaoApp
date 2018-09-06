a0dc6c53de92c7de0c143680d7784c1a946e1a45
515074431 <515074431@qq.com>
2018-09-06 16:50:15 +0800

    um苹果的也好了

--- ios/hstApp/AppDelegate.m
+++ ios/hstApp/AppDelegate.m
@@ -10,11 +10,19 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "SplashScreen.h"  // here
+#import "RNUMConfigure.h"
+#import <UMAnalytics/MobClick.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [UMConfigure setLogEnabled:YES];
+  
+  /* Umeng init */
+  [MobClick setScenarioType:E_UM_GAME|E_UM_DPLUS];
+  [RNUMConfigure initWithAppkey:@"5b8f98c2f29d98484500025c" channel:@"App Store"];
+
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
